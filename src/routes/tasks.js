ecbe771f64d9d8366dcc1ec0a2b9096ad8f8eaa3
AlejoFangamer <xxalejoxx71@gmail.com>
2023-09-23 12:32:37 -0500

    si

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,28 +1,74 @@
 import { Router } from "express";
 import { pool } from "../db.js";
 
-export const tasksRouter  = Router();
+export const tasksRouter = Router();
 
-tasksRouter.get('/',async (req,res) => {
+tasksRouter.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM tasks");
-    res.json(result.rows);
+    res.status(200).json(result.rows);
   } catch (err) {
     console.log(err);
-    res.status(500).json({error: err.message});
-  }  
+    res.status(500).json({ error: err.message });
+  }
 });
 
-tasksRouter.get('/:id',async (req,res) => {
-  const id = req.params.id;
+tasksRouter.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
   try {
-    const result = await pool.query(`SELECT * FROM tasks WHERE id_task = ${id}`);
+    const result = await pool.query(`SELECT * FROM tasks WHERE id_task = $1`, [
+      id,
+    ]);
+
     if (result.rows.length <= 0) {
-      return res.status(404).json({error: "No se encontró el elemento"})
+      return res.status(404).json({ error: "No se encontró el elemento" });
     }
+
     res.json(result.rows[0]);
   } catch (err) {
     console.log(err);
-    res.status(500).json({error: err.message});
-  }  
+    res.status(500).json({ error: err.message });
+  }
+});
+
+tasksRouter.post("/", async (req, res) => {
+  try {
+    const { title } = req.body;
+    const create = await pool.query(
+      "INSERT INTO tasks (title) VALUES ($1) RETURNING *",
+      [title],
+    );
+    res.status(201).json(create.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+tasksRouter.put("/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const { title, completed } = req.body;
+
+    const result = pool.query(
+      "UPDATE tasks SET title = $1, completed = $2 WHERE id_task = $3",
+      [title, completed, id]
+    );
+
+    res.status(200).send(`Tarea con el ID: ${id} fue modificada exitosamente`)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+tasksRouter.delete("/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    await pool.query("DELETE FROM tasks WHERE id_task = $1", [id]);
+    res.status(200).send(`Tarea con el ID: ${id} fue borrada exitosamente`)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
 });
